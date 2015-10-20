80ae88dbe39c1d74429e63b20f7cd1bc5a1dec6b
Marco Calabretta <marco.calabretta@accenture.com>
2015-10-20 12:12:22 +0200

    Add web module

---
+++ earlywarning-pom/earlywarning-ws-web/src/main/java/com/isp/service/ExCall.java
@@ -0,0 +1,76 @@
+package com.isp.service;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import com.i4c.antares.taskmanager.workflow.WorkflowException;
+import com.i4c.antares.taskmanager.workflow.WorkflowFacadeLocal;
+import com.i4c.antares.taskmanager.workflow.WorkflowParameters;
+import com.i4c.antares.taskmanager.workflow.WorkflowRepositoryLocal;
+import com.i4c.antares.taskmanager.workflow.WorkflowRun;
+import com.i4c.antares.taskmanager.workflow.WorkflowRunResult;
+import com.i4c.antares.usersactions.business.UserManager;
+
+@WebService(name = "executeDailyBatch", serviceName = "executeDailyBatchWS")
+@Stateless
+public class ExCall {
+
+	private static final String USER_ID = "admin";
+
+	@EJB
+	WorkflowRepositoryLocal workflowRepository;
+	@EJB
+	UserManager userManager;
+	@EJB
+	WorkflowFacadeLocal executor;
+
+	@WebMethod
+	@WebResult(name = "Result")
+	public String executeWorkflow(String workflowName) {
+		StringBuffer message = new StringBuffer();
+		WorkflowRunResult runResult;
+		try {
+
+			if (!executor.existsWorkflow(workflowName)) {
+				message.append("Error - workflow not exist " + workflowName);
+			} else {
+
+				if (!isRunning(workflowName)) {
+					runResult = executor.executeWorkflowByName(workflowName,
+							new WorkflowParameters(), USER_ID);
+
+					if (runResult.allStepRunsSuccessful()) {
+						message.append("WorkFlow executed");
+					} else {
+						message.append("Error - execute workflow");
+					}
+				}
+				else
+					message.append("Error - workflow running");
+			}
+		} catch (WorkflowException e) {
+			message.append("Error " + e.getMessage());
+		}
+
+		return message.toString();
+	}
+
+	private boolean isRunning(String workflowName) {
+		List<WorkflowRun> workflowRuns = executor.findAllRunningWorkflowRuns();
+
+		for (WorkflowRun run : workflowRuns) {
+			if (run.getWorkflow().getName().equalsIgnoreCase(workflowName)) {
+				return true;
+			}
+
+		}
+
+		return false;
+	}
+
+}
